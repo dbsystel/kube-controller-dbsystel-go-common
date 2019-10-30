3c46fcc4267d5065af7846b668a896506e612d42
Kirchen99 <latias_latios@126.com>
2019-10-30 11:18:43 +0100

    fix: fix list & watch func

--- controller/service/service.go
+++ controller/service/service.go
@@ -32,10 +32,10 @@ func (sc *ServiceController) Initialize(kclient *kubernetes.Clientset) {
 	informer := cache.NewSharedIndexInformer(
 		&cache.ListWatch{
 			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
-				return kclient.CoreV1().Service(metav1.NamespaceAll).List(options)
+				return kclient.CoreV1().Services(metav1.NamespaceAll).List(options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
-				return kclient.CoreV1().Service(metav1.NamespaceAll).Watch(options)
+				return kclient.CoreV1().Services(metav1.NamespaceAll).Watch(options)
 			},
 		},
 		&v1.Service{},
