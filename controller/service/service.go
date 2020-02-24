b527eddfa371662c6872fedab3991d72872a5b4a
Lunjie Zhang <lunjie.zhang-extern@deutschebahn.com>
2020-02-24 17:04:41 +0100

    feat: Added service controller, changed ingress apigroup (by external expert on behalf of DB Systel GmbH)

--- controller/service/service.go
+++ controller/service/service.go
@@ -1,11 +1,11 @@
-package ingress
+package service
 
 import (
 	"sync"
 	"time"
 
 	"github.com/dbsystel/kube-controller-dbsystel-go-common/controller"
-	"k8s.io/api/networking/v1beta1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
@@ -13,43 +13,43 @@ import (
 	"k8s.io/client-go/tools/cache"
 )
 
-type IngressController struct {
+type ServiceController struct {
 	Controller controller.Controller
 	informer   cache.SharedIndexInformer
 	kclient    *kubernetes.Clientset
 }
 
-func (ic *IngressController) Run(stopCh <-chan struct{}, wg *sync.WaitGroup) {
+func (sc *ServiceController) Run(stopCh <-chan struct{}, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	wg.Add(1)
-	go ic.informer.Run(stopCh)
+	go sc.informer.Run(stopCh)
 	<-stopCh
 }
 
-func (ic *IngressController) Initialize(kclient *kubernetes.Clientset) {
+func (sc *ServiceController) Initialize(kclient *kubernetes.Clientset) {
 
 	informer := cache.NewSharedIndexInformer(
 		&cache.ListWatch{
 			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
-				return kclient.NetworkingV1beta1().Ingresses(metav1.NamespaceAll).List(options)
+				return kclient.CoreV1().Services(metav1.NamespaceAll).List(options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
-				return kclient.NetworkingV1beta1().Ingresses(metav1.NamespaceAll).Watch(options)
+				return kclient.CoreV1().Services(metav1.NamespaceAll).Watch(options)
 			},
 		},
-		&v1beta1.Ingress{},
+		&v1.Service{},
 		3*time.Minute,
 		cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc},
 	)
 
 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    ic.Controller.Create,
-		UpdateFunc: ic.Controller.Update,
-		DeleteFunc: ic.Controller.Delete,
+		AddFunc:    sc.Controller.Create,
+		UpdateFunc: sc.Controller.Update,
+		DeleteFunc: sc.Controller.Delete,
 	})
 
-	ic.informer = informer
-	ic.kclient = kclient
+	sc.informer = informer
+	sc.kclient = kclient
 
 }
